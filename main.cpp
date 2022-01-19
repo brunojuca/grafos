cabc4f5bd25112896dd0fd5ee9504528a1cfdfc9
Bruno Juca <brunoolijuca@gmail.com>
2022-01-19 16:15:55 -0300

    Leitura das novas instâncias implementada

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include "Utils.h"
+#include "Graph.h"
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout << "New main test" << endl;
+
+    if (argc < 6)
+    {
+        cout << "Poucos argumentos na chamada do programa" << endl;
+        cout << "Siga as instruções do arquivo README.md" << endl;
+        return 1;
+    }
+    else if (argc > 6)
+    {
+        cout << "Muitos argumentos na chamada do programa" << endl;
+        cout << "Siga as instruções do arquivo README.md" << endl;
+    }
+
+    Graph *graph = Utils::createGraphPart2(argv[1], stoi(argv[3]), stoi(argv[4]), stoi(argv[5]));
+
     return 0;
 }
